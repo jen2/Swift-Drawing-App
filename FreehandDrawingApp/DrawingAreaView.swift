c9004f1df7dbe7a5389ed1616dafab5ccb912b7d
jen2 <sipila.jen@gmail.com>
2016-11-03 21:58:04 -0400

    Created initial main view controller and drawing area view classes, set up storyboard with drawing area and button controls

---
+++ FreehandDrawingApp/DrawingAreaView.swift
@@ -0,0 +1,14 @@
+//
+//  DrawingAreaView.swift
+//  FreehandDrawingApp
+//
+//  Created by Jennifer A Sipila on 11/3/16.
+//  Copyright © 2016 Jennifer A Sipila. All rights reserved.
+//
+
+import UIKit
+
+class DrawingAreaView : UIView {
+    
+    
+}
---
+++ FreehandDrawingApp/MainDrawingViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MainDrawingViewController.swift
+//  FreehandDrawingApp
+//
+//  Created by Jennifer A Sipila on 11/3/16.
+//  Copyright © 2016 Jennifer A Sipila. All rights reserved.
+//
+
+import UIKit
+
+class MainDrawingViewController : UIViewController
+{
+    
+    @IBAction func colorButtonTapped(_ sender: UIButton) {
+    }
+    
+    @IBAction func eraseButtonTapped(_ sender: UIButton) {
+    }
+    
+    @IBAction func lineWidthSliderMoved(_ sender: UISlider) {
+    }
+    
+    @IBAction func clearButtonTapped(_ sender: UIButton) {
+    }
+    
+    @IBAction func saveButtonTapped(_ sender: UIButton) {
+    }
+    
+}
